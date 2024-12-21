792fe73d4b8fe4478a4852501c3c03d9daa29470
EarthlyEric <earthlyeric6@gmail.com>
2024-12-21 21:08:22 +0800

    add page 3 4 5

--- page3.py
+++ page3.py
@@ -1,15 +1,23 @@
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import os
+import numpy as np
+import matplotlib.pyplot as plt
 
-cleanedDataset = pd.read_csv('spotify_tracks_cleaned.csv')
+dataset = pd.read_csv('spotify_tracks.csv')
+average = {}
 
-yearReleased = cleanedDataset['year']
+useColumns = ['danceability', 'acousticness', 'duration_ms', 'energy', 'instrumentalness', 'key', 'liveness', 'loudness', 'mode', 'speechiness', 'tempo', 'valence']
+for column in useColumns:
+    average[column] = np.mean(dataset[column])
 
-Bar = plt.bar(yearReleased.value_counts().index, yearReleased.value_counts().values, color='skyblue')
-plt.xlabel('Release Year')
-plt.ylabel('Number of Songs')
-plt.title('Number of Songs Released on Spotify Every Year')
-plt.savefig(os.path.join('output','Number of Songs Released on Spotify Every Year.png'))
-plt.show()
+print("平均適合跳舞程度: ", average['danceability'])
+print("平均聲學性: ", average['acousticness'])
+print("平均歌曲時長: ",  f"{int(average['duration_ms'] / 1000 // 60)} 分 {int(average['duration_ms'] / 1000 % 60)} 秒")
+print("平均能量: ", average['energy'])
+print("平均演奏性: ", average['instrumentalness'])
+print("平均音調: ", average['key'])
+print("平均現場感: ", average['liveness'])
+print("平均響度: ", average['loudness'], "dB")
+print("平均調性模式: ", average['mode'])
+print("平均語音性: ", average['speechiness'])
+print("平均節奏: ", average['tempo'], "bpm")
+print("平均情感: ", average['valence'])
---
+++ page5.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load the dataset
+dataset = pd.read_csv('spotify_tracks.csv')
+
